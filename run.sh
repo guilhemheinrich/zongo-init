dbbf68def9a6f5920ae6f9abef13449a7fe5d258
root <root@localhost.localdomain>
2018-05-25 22:44:26 +0000

    uncomment

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DIR="`dirname \"$0\"`"
 
-# sh $DIR/init-scripts/setup-bashrc.sh
-# sh $DIR/init-scripts/setup-smtp.sh
+sh $DIR/init-scripts/setup-bashrc.sh
+sh $DIR/init-scripts/setup-smtp.sh
 sh $DIR/init-scripts/setup-hacktools.sh
