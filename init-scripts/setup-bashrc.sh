77062fdd2ef316661296af3cec4293c482c788c6
root <root@localhost.localdomain>
2018-05-25 22:16:11 +0000

    small fixes

--- init-scripts/setup-bashrc.sh
+++ init-scripts/setup-bashrc.sh
@@ -5,7 +5,7 @@ sed -i "/HISTFILESIZE/ s|=.*|=-1|g" $HOME/.bashrc
 # Identical to
 # sed -i "s|\(HISTSIZE=\).*|\1-1|g" $HOME/.bashrc
 # sed -i "s|\(HISTFILESIZE=\).*|\1-1|g" $HOME/.bashrc
-echo "setxkbmap fr" >> $HOME/.bashrc
+printf "\nsetxkbmap fr\n" >> $HOME/.bashrc
 
 
 printf "alias hh=\"history | grep\"\n" >> $HOME/.bash_aliases
